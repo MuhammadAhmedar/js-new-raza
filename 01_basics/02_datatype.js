f26eec0cb92b4f91b2d46ac8dba8e727e990b009
Ahmed raza <152934392+MuhammadAhmedar@users.noreply.github.com>
2023-12-12 21:33:23 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,15 @@
+"use strict";/*treat all js code as a newer version */
+//alert(3+3) //we are using node js,not browser
+
+
+let age =26
+let isloggedIn = false
+let state = null
+
+
+//number => 2 to power 53
+//bigint
+//string =>""
+//bolean =>true/false
+//null => standealone vale
+// undefiend =>
