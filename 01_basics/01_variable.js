ed7e5b555fb732d50630b9031fd033e35b129845
Ahmed raza <152934392+MuhammadAhmedar@users.noreply.github.com>
2023-12-12 20:35:54 +0000

    container confg done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,20 @@
+const accountID = 123456 
+let accountEmail ="raza@gmail.com"
+var accountPassword ="12345"
+accountCity ="karachi"
+let accountState;
+
+
+// const accountID = 5675
+accountEmail ="ahmed@gmail.com"
+accountPassword ="543321"
+accountCity ="islamabad"
+
+
+
+
+
+
+
+
+console.table([accountID,accountEmail,accountPassword,accountCity,accountState]);
