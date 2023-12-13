a2d524645d65da55507d04650fdf5de4beb8d65e
Ahmed raza <152934392+MuhammadAhmedar@users.noreply.github.com>
2023-12-13 20:32:35 +0000

    convertion of datatypes

---
+++ 01_basics/03_convertionOperations.js
@@ -0,0 +1,27 @@
+let score = "ahmed"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number (score)
+//console.log(typeof (valueInNumber));
+//console.log(valueInNumber);
+
+// "33" => 33
+//  "33aabc" => NaN
+// trur =>1, false => 0
+
+let IsLogIn = "ahmed"
+
+let boleanIsLogIn = Boolean (IsLogIn)
+//console.log(boleanIsLogIn);
+
+// 1 => true 0=>false
+// "" => false
+// "Ahmed" => true
+
+let SomeNumber = 33
+
+let StringSomeNumber = String (SomeNumber);
+console.log(SomeNumber);
+console.log(typeof SomeNumber);
