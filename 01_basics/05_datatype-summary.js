ba1611121fc7bb060f46c12bac4a8f460e3edc01
Ahmed raza <152934392+MuhammadAhmedar@users.noreply.github.com>
2023-12-14 01:10:37 +0000

    datatype of summary

---
+++ 01_basics/05_datatype-summary.js
@@ -0,0 +1,22 @@
+//.  pirimitive
+
+// 7 types: string, number, bolean, null, undefined, symbol, bigInt
+
+const score = 100
+const scoreValue = 100.56
+
+const isLognIn = true
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber =123456788976565318282n
+
+
+//   Reference (Non pirimitive)
+
+//  arry, objects, functions
