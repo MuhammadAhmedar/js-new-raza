9461423bdca88d0ad429190c36e746dd062c3f1b
Ahmed raza <152934392+MuhammadAhmedar@users.noreply.github.com>
2023-12-14 21:24:03 +0000

    string

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,28 @@
+const name = "Ahmed"
+const repoCount = 10
+
+//console.log(`hello my name is ${name} and my repoCount is ${repoCount}`);
+
+const gameName = new String ('Ahmed')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.indexOf('e'));
+// console.log(gameName.charAt(3));
+
+// const newString = gameName.substring(0,4) 
+// console.log(newString);
+// const anotherString = gameName.slice(0,-3) 
+// console.log(anotherString);
+
+const newStringOne = "    Ahmed    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+
+const url = "http//:www.ahmed.com/ahmed%209raza"
+
+console.log(url.replace('%209','-'));
