712d44df80a1b14c5c4dcbdbdccb5a5ea2d84454
Ahmed raza <152934392+MuhammadAhmedar@users.noreply.github.com>
2023-12-21 00:55:23 +0000

    arry

---
+++ 02_basics/01_arry.js
@@ -0,0 +1,35 @@
+// Arry 
+
+const myArry = [0, 1, 2, 3, 4, 5]
+const myHero = ["superman", "batman"]
+
+const myArry2 = new Array (1, 2, 3, 4);
+// console.log(myArry2[1]);
+
+// Arry methods
+
+// myArry.push (6)
+// myArry.push (7)
+// myArry.pop ()
+
+// myArry.unshift (9)  //unrecommended
+// myArry.shift (9)
+
+// console.log(myArry.includes(9));
+// console.log(myArry.indexOf(9));
+
+// const newArry = myArry.join()
+// console.log(myArry);
+// console.log(newArry);
+
+console.log("A" ,myArry);
+const mAn1 = myArry.slice(1, 3)
+
+console.log(mAn1);
+console.log("B", myArry);
+
+const mAn2 = myArry.splice(1, 3)
+console.log("C",myArry);
+console.log(mAn2);
+
+
---
+++ 02_basics/02_aryy.js
@@ -0,0 +1,6 @@
+const marvals_heros = [ "IronMan", "SpiderMan", "AntMan" ]
+const dc_heros = ["superman", "flas","batman" ]
+
+marvals_heros.push(dc_heros)
+
+console.log(marvals_heros);
