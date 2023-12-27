be030992f4b2fc3c7694a24fd3598a7d3a5aa012
Ahmed raza <152934392+MuhammadAhmedar@users.noreply.github.com>
2023-12-27 00:49:46 +0000

    function in js

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,39 @@
+var c = 300
+let = 300
+if (true) {
+    a =10
+    const b =20
+//    console.log("INNER:" ,a);
+}
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one() {
+    //username = "Ahmed"
+
+    function two() {
+        const website ="youtube"
+       // console.log(username);
+    }
+    //console.log(website);
+    two()
+}
+one()
+
+if (true) {
+    const username = "Ahmed"
+    if (username === "Ahmed") {
+        const website = " youtube"
+        console.log(username + website);
+    }
+  //  console.log(website);
+}
+// console.log(username);
+addone ()
+function addone(num) {
+    return num + 1
+}
+const addtwo =function(num) {
+   return num + 2
+}
