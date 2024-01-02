fc97293c4bd79159283ed8e4a1dda4f65605e95c
Ahmed raza <152934392+MuhammadAhmedar@users.noreply.github.com>
2024-01-02 22:57:17 +0000

    one js

---
+++ 04_control_flow/one.js
@@ -0,0 +1,45 @@
+// const isUserLoggedIn = true
+// const temperature = 51
+
+// if (temperature===41) {
+//     console.log("less than 50");
+// } else{
+//     console.log("temperature is greater than 50");
+// }
+
+// <, >, <=, >=, ==, ===, !== ///
+
+// const score = 200
+// if (score>100) {
+//     const power = "fly"
+//     console.log(`user power: ${power}`);
+// } 
+// console.log(`user power: ${power}`);
+
+// const balance =1000
+// // if (balance>500) console.log("test"),console.log("test2");
+// if (balance < 500) {
+//     console.log("less then 500");
+// }
+
+// else if(balance < 750){
+//     console.log("less then 750");
+// }
+ 
+// else if(balance < 900){
+//     console.log("less then 900");
+// } else{
+//     console.log("less than 1200");
+// }
+
+// const userLoggedIn =true
+// const debietCard =true
+// const loggedInFromGoggle= false
+// const LoggedInFromEmail =true
+
+// if (userLoggedIn && debietCard && 2==3){
+// console.log("Allow to buy course");
+// }
+// if (loggedInFromGoggle || LoggedInFromEmail){
+//     console.log("user logged In");
+// }
