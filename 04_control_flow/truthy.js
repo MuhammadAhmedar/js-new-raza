822d9eca373db1a9808befa051991fef1e816985
Ahmed raza <152934392+MuhammadAhmedar@users.noreply.github.com>
2024-01-03 23:24:56 +0000

    truthy in js

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,41 @@
+const userEmail = ""
+
+// if (userEmail) {
+//     console.log("Got your Email");
+// } else {
+//     console.log("Don’t have user email ");
+// }
+
+// falsy values
+
+// falsy, 0, -0, bigInt 0n, "", null, undefined, Nan
+
+// truthy values
+// "0", 'false', " ", [], {}, function(){}
+
+// if (userEmail.length === 0) {
+//     console.log("Array is empty");
+// }
+// const emptyObj = {}
+
+// if (Object.keys(emptyObj).length === 0) {
+//     console.log("Object is empty");
+// }
+
+// Nullish Coalescing Oprater
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 =undefined ?? 10
+// val1 =null ?? 10 ?? 20
+
+
+// console.log(val1);
+
+// Terniary Oprater
+
+// Condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <= 80 ? console.log("less then 80") : console.log("more then 80");
