177040eab90966ed22828549fc9abe9bd247b0ef
Ahmed raza <152934392+MuhammadAhmedar@users.noreply.github.com>
2024-01-03 00:54:41 +0000

    Switch in js

---
+++ 04_control_flow/Switch.js
@@ -0,0 +1,54 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month = 4
+
+// switch (month) {
+//     case 1:
+//         console.log("month is January");
+//         break;
+//     case 2:
+//         console.log("month is February");
+//         break;
+//     case 3:
+//         console.log("month is march");
+//         break;
+//     case 4:
+//         console.log("month is April ");
+//         break;
+//     case 5:
+//         console.log("month is may");
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month ="aprail"
+
+switch (month) {
+    case "jan":
+        console.log("month is January");
+        break;
+    case "feb":
+        console.log("month is February");
+        break;
+    case "mar":
+        console.log("month is march");
+        break;
+    case "aprail":
+        console.log("month is April ");
+        break;
+    case "may":
+        console.log("month is may");
+        break;
+
+    default:
+        break;
+}
